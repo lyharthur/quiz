1ecd3a0a9edeed176435d5f54f91f1da4b31e0ef
lyharthur <lyharthur@gmail.com>
2015-10-16 00:03:52 +0800

    q2 first version compare the character one by one. Time Complexity O(n) Can try the binary search.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,23 @@
-/* FIXME: Implement! */
-
-int main()
-{
-    return 0;
+#include<stdio.h>
+char smallest_character(char str[], char c) {
+    int i = 0;
+    while(str[i] != '\0'){
+        if(c < str[i])
+            return str[i];
+        i++;
+    }
+    return str[0];
+}
+int main(){
+    char str[5]="cfjpv";
+    char str2[3]="cck";
+    if ('c' == smallest_character(str,'a'))
+        printf("pass case1\n");
+    if ('f' == smallest_character(str,'c'))
+        printf("pass case2\n");
+    if ('c' == smallest_character(str,'z'))
+        printf("pass case3\n");
+    if ('k' == smallest_character(str2,'f'))
+        printf("pass case4\n");
+    return 0 ;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,19 @@
-/* FIXME: Implement! */
-
-int main()
-{
-    return 0;
+#include<stdio.h>
+char smallest_character_r(char str[], char c, int index) {
+    if(str[index] == '\0') return str[0];
+    if(str[index] > c) return str[index];
+    else  return smallest_character_r(str,c,index+1);
+}
+int main(){
+    char str[5]="cfjpv";
+    char str2[3]="cck";
+    if ('c' == smallest_character_r(str,'a',0))
+        printf("pass case1_r\n");
+    if ('f' == smallest_character_r(str,'c',0))
+        printf("pass case2_r\n");
+    if ('c' == smallest_character_r(str,'z',0))
+        printf("pass case3_r\n");
+    if ('k' == smallest_character_r(str2,'f',0))
+        printf("pass case4_r\n");
+    return 0 ;
 }
