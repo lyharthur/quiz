b589e998fdd2be904d7244bf498b1dfd1a93204a
lyharthur <lyharthur@gmail.com>
2015-10-21 23:13:25 +0800

    add the test func to vaild the detectCylce() use astyle to fix the coding style

--- q5/iterative.c
+++ q5/iterative.c
@@ -5,15 +5,14 @@ typedef struct ListNode {
     int val;
     struct ListNode *next;
 } ListNode;
-ListNode *head=NULL;
-ListNode *tail=NULL;
+
 ListNode *detectCycle(ListNode *head)
 {
     struct ListNode *node_f = head;
     struct ListNode *node_s = head;
     while(node_f && node_s) {
         node_f = node_f->next;
-        node_s = node_s->next;
+        node_s = node_s->next->next;
         if(node_f == node_s)
             break;
     }
@@ -24,8 +23,74 @@ ListNode *detectCycle(ListNode *head)
         node_s = node_s->next;
     }
     return node_f;
+
 }
+
+void traverse_list(struct ListNode *head)
+{
+    while(head) {
+        printf("%d\n", head->val);
+        head = head->next;
+    }
+    return;
+}
+
+
+struct ListNode* create_node(int val)
+{
+    struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+    new_node->val = val;
+    new_node->next = NULL;
+    return new_node;
+}
+
+
+struct ListNode *create_cycle_list(int cycle_pos, int size)
+{
+    struct ListNode *list_head = create_node(0);
+    struct ListNode* parse = list_head;
+
+    if (cycle_pos > size) return NULL;
+
+    for (int i = 1; i <= size; i++) {
+        if (i == cycle_pos) list_head = parse;
+        parse->next = create_node(i);
+        parse = parse->next;
+    }
+    parse->next = list_head->next;
+
+    return list_head;
+}
+
+
+struct ListNode *create_linear_list(int size)
+{
+    struct ListNode *list_head = create_node(0);
+    struct ListNode *parse = list_head;
+
+    for(int i = 1; i <= size; i++) {
+        parse->next = create_node(i);
+        parse = parse->next;
+    }
+    return list_head;
+}
+
+
+
 int main()
 {
 
+    struct ListNode *cycle_list = create_cycle_list(1, 6);
+    struct ListNode *linear_list = create_linear_list(5);
+
+    /* detect cycle */
+    if(detectCycle(NULL))
+        printf("case 4 : has cycle\n");
+    if(detectCycle(linear_list))
+        printf("case 2 : has cycle\n");
+    if(detectCycle(cycle_list))
+        printf("case3 : has cylce\n");
+
+    /* free memory */
+    return 0;
 }
